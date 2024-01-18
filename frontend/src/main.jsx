afffe9a3e759179ff02fba7eed025e80c7255ae3
Techer Tony <techer307@gmail.com>
2024-01-18 10:32:38 +0100

    nothing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       {
-        path: "/Accueil",
+        index: true,
         element: <Accueil />,
       },
     ],
